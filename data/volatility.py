bf6cb74ac014eae46e02503fb3a6bb9926e762fc
brunogong <brunogong@gmail.com>
2026-04-03 22:11:51 +0200

    Create volatility.py

---
+++ data/volatility.py
@@ -0,0 +1,6 @@
+def calc_atr(df, period=14):
+    df['H-L'] = df['High'] - df['Low']
+    df['H-PC'] = abs(df['High'] - df['Close'].shift(1))
+    df['L-PC'] = abs(df['Low'] - df['Close'].shift(1))
+    tr = df[['H-L','H-PC','L-PC']].max(axis=1)
+    return tr.rolling(period).mean().iloc[-1]
