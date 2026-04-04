643cf4d4e254285362c0cd51ca2f078672173397
brunogong <brunogong@gmail.com>
2026-04-04 13:12:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from data.cot import get_cot
 from data.retail import myfxbook_login, get_retail_sentiment
 from data.technical import get_ohlc, get_rsi_multi_tf_from_df
 from data.volatility import get_atr_from_df
-from utils.mapping import FINNHUB_MAPPING
+from utils.mapping import ALPHA_MAPPING
 
 from logic.signals import generate_signal
 from ui.dashboard import render_signal_card
@@ -38,12 +38,9 @@ def main():
         # 1) COT reale
         cot = get_cot(pair)
 
-        # 2) OHLC da Finnhub
-        symbol = FINNHUB_MAPPING[pair]
-        df = get_ohlc(symbol, "60")  # 60 = 1H
-        
-
-
+        # 2) OHLC da Alpha Vantage
+        symbol_tuple = ALPHA_MAPPING[pair]
+        df = get_ohlc(symbol_tuple)
 
         # 3) RSI multi‑TF locale
         rsi = get_rsi_multi_tf_from_df(df)
