cad83a0773f382fd9b6bdd69526a814b3fea8607
brunogong <brunogong@gmail.com>
2026-04-04 13:10:01 +0200

    Update mapping.py

--- utils/mapping.py
+++ utils/mapping.py
@@ -1,16 +1,8 @@
-# Mappatura simboli per Finnhub (OHLC)
-# ------------------------------------
-# Finnhub usa il formato:
-# FOREX:EURUSD
-# FOREX:GBPUSD
-# FOREX:USDJPY
-# FOREX:XAUUSD
-
-FINNHUB_MAPPING = {
-    "EURUSD": "FOREX:EURUSD",
-    "GBPUSD": "FOREX:GBPUSD",
-    "USDJPY": "FOREX:USDJPY",
-    "XAUUSD": "FOREX:XAUUSD"
+ALPHA_MAPPING = {
+    "EURUSD": ("EUR", "USD"),
+    "GBPUSD": ("GBP", "USD"),
+    "USDJPY": ("USD", "JPY"),
+    "XAUUSD": ("XAU", "USD")
 }
 
 
