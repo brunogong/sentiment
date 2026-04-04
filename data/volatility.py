5b2d6c96b819bb596c5ee06a5e00a321642d3ec8
brunogong <brunogong@gmail.com>
2026-04-04 12:11:24 +0200

    Update volatility.py

--- data/volatility.py
+++ data/volatility.py
@@ -1,6 +1,13 @@
-def calc_atr(df, period=14):
-    df['H-L'] = df['High'] - df['Low']
-    df['H-PC'] = abs(df['High'] - df['Close'].shift(1))
-    df['L-PC'] = abs(df['Low'] - df['Close'].shift(1))
-    tr = df[['H-L','H-PC','L-PC']].max(axis=1)
-    return tr.rolling(period).mean().iloc[-1]
+from data.technical import get_ohlc
+
+def get_atr(symbol, interval="1h", period=14):
+    df = get_ohlc(symbol, interval, outputsize=period*3)
+
+    df["H-L"] = df["high"] - df["low"]
+    df["H-PC"] = abs(df["high"] - df["close"].shift(1))
+    df["L-PC"] = abs(df["low"] - df["close"].shift(1))
+
+    tr = df[["H-L", "H-PC", "L-PC"]].max(axis=1)
+    atr = tr.rolling(period).mean().iloc[-1]
+
+    return float(atr)
